0371a4d9e5f62f63d8bb0e41ddcd687f47461ce0
IngvaldssonMattias <mattias.ingvaldsson@chasacademy.se>
2025-11-17 09:53:10 +0100

    lagt in component.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-import { getWeatherFromCity} from "./services/services.js"; // importerar funktionen från services.js
+import { getWeatherFromCity } from "./services/services.js";
 
-const searchButton = document.getElementById('search-button'); // skapar variabel searchButton och hämtar det från id i HTML
-const searchField = document.getElementById('search-field'); // skapar variabel searchField och hämtar det från id i HTML
-const weatherResult = document.getElementById('display-text'); // skapar variabel weatherResult och hämtar det från id i HTML
+const searchButton = document.getElementById('search-button');
+const searchField = document.getElementById('search-field');
+const weatherResult = document.getElementById('display-text');
 
-searchButton.addEventListener('click', async () => { // Lägger till en 'click'-eventlistener på searchButton
-    const city = searchField.value; // hämtar det användaren har skrivit in i sökfältet
-    console.log(`Söker väder för stad: ${city}`);
-    const weather = await getWeatherFromCity(city)
+searchButton.addEventListener('click', async () => {
 
-   return weatherResult.innerHTML = `<p>Today at ${weather.time} ${weather.timeZone} in ${weather.city[0].toUpperCase() + weather.city.slice(1).toLowerCase()} the temperature is ${weather.temperature} degrees celsius and the weather is ${weather.weather.toLowerCase()}.</p>`;
+    const city = searchField.value;
+    const weather = await getWeatherFromCity(city);
 
+    document.getElementById("temp").textContent = weather.temperature;
+    document.getElementById("unit").textContent = "°C";
+    document.getElementById("weather").textContent = weather.weather;
+    document.getElementById("location").textContent = weather.city;
+  
+
+
+    // weatherResult.innerHTML = `
+    //     <p>Today at ${weather.time} ${weather.timeZone} in 
+    //     ${weather.city[0].toUpperCase() + weather.city.slice(1).toLowerCase()} 
+    //     the temperature is ${weather.temperature} degrees celsius 
+    //     and the weather is ${weather.weather.toLowerCase()}.</p>`;
 });
